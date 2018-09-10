a0d0f7cfecffb0dbe2b7e9f5764c17a6793433ff
Satia Herfert <satiaherfert@gmx.de>
2018-09-10 11:33:21 +0200

    Move PeriodicCommitHintTest over.

---
+++ parser/src/test/scala/org/opencypher/v9_0/it/PeriodicCommitHintIT.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2002-2018 Neo4j Sweden AB (http://neo4j.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opencypher.v9_0.it
+
+import org.opencypher.v9_0.ast
+import org.opencypher.v9_0.ast.PeriodicCommitHint
+import org.opencypher.v9_0.expressions.SignedDecimalIntegerLiteral
+import org.opencypher.v9_0.parser.ParserTest
+import org.opencypher.v9_0.parser.Query
+import org.opencypher.v9_0.util.DummyPosition
+import org.parboiled.scala._
+
+class PeriodicCommitHintIT extends ParserTest[ast.PeriodicCommitHint, Any] with Query {
+
+  implicit val parserToTest: Rule1[PeriodicCommitHint] = PeriodicCommitHint ~ EOI
+
+  val t = DummyPosition(0)
+
+  test("tests") {
+    parsing("USING PERIODIC COMMIT") shouldGive ast.PeriodicCommitHint(None)(t)
+    parsing("USING PERIODIC COMMIT 300") shouldGive ast.PeriodicCommitHint(Some(SignedDecimalIntegerLiteral("300")(t)))(t)
+  }
+
+  override def convert(astNode: ast.PeriodicCommitHint): Any = astNode
+}
