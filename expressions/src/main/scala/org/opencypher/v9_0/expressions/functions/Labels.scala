efc648668a6b1acae24b27e81f38ed63d1d346c6
Sascha Peukert <sascha.peukert@neotechnology.com>
2019-01-28 15:14:34 +0100

    Fix problems with distance() and labels()

--- expressions/src/main/scala/org/opencypher/v9_0/expressions/functions/Labels.scala
+++ expressions/src/main/scala/org/opencypher/v9_0/expressions/functions/Labels.scala
@@ -22,6 +22,6 @@ case object Labels extends Function with TypeSignatures {
   override def name = "labels"
 
   override val signatures = Vector(
-    TypeSignature(CTNode, CTString, "Returns a list containing the string representations for all the labels of a node.")
+    TypeSignature(CTNode, CTList(CTString), "Returns a list containing the string representations for all the labels of a node.")
   )
 }
