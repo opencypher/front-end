2541ab51c7bfe42f02590334b763202a42ff6571
Therese Magnusson <scout.therese@gmail.com>
2019-05-13 11:04:48 +0200

    Addressed review comments and fixed failing test after rebase

--- ast/src/main/scala/org/opencypher/v9_0/ast/CatalogDDL.scala
+++ ast/src/main/scala/org/opencypher/v9_0/ast/CatalogDDL.scala
@@ -67,7 +67,7 @@ final case class CreateUser(userName: String,
 
 final case class DropUser(userName: String)(val position: InputPosition) extends MultiDatabaseDDL {
 
-  override def name = "CATALOG CREATE USER"
+  override def name = "CATALOG DROP USER"
 
   override def semanticCheck: SemanticCheck =
     super.semanticCheck chain
