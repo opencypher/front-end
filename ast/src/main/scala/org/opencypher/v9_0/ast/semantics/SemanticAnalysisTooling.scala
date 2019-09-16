f46fcf575ca857c9f30f25e3047e95a472a98002
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-09-16 09:41:04 +0200

    Allow arbitrary checked expressions in view invocations with feature flag

--- ast/src/main/scala/org/opencypher/v9_0/ast/semantics/SemanticAnalysisTooling.scala
+++ ast/src/main/scala/org/opencypher/v9_0/ast/semantics/SemanticAnalysisTooling.scala
@@ -132,6 +132,12 @@ trait SemanticAnalysisTooling {
     else
       SemanticCheckResult.success(state)
 
+  def whenState(condition: SemanticState => Boolean)(check: => SemanticCheck): SemanticCheck = state =>
+    if (condition(state))
+      check(state)
+    else
+      SemanticCheckResult.success(state)
+
   def unless(condition: Boolean)(check: => SemanticCheck): SemanticCheck = state =>
     if (condition)
       SemanticCheckResult.success(state)
