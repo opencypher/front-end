9dee386b95d25a8e9bbdbd6fdf4362d1097bcdfc
Louise Söderström <louise.matematik@gmail.com>
2020-01-08 10:51:15 +0100

    New year, new license

--- ast/src/main/scala/org/opencypher/v9_0/ast/ASTSlicingPhrase.scala
+++ ast/src/main/scala/org/opencypher/v9_0/ast/ASTSlicingPhrase.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2002-2019 Neo4j Sweden AB (http://neo4j.com)
+ * Copyright © 2002-2020 Neo4j Sweden AB (http://neo4j.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
