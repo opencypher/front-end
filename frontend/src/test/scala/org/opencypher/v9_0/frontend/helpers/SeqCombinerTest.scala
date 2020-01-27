d9a25e775a9db5d4042442cfb2d4797819c40c32
Pontus Melke <pontusmelke@gmail.com>
2020-01-27 12:51:24 +0100

    Code review: faster combiner for arrays

---
+++ frontend/src/test/scala/org/opencypher/v9_0/frontend/helpers/SeqCombinerTest.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2002-2020 Neo4j Sweden AB (http://neo4j.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opencypher.v9_0.frontend.helpers
+
+import org.opencypher.v9_0.frontend.helpers.SeqCombiner.combine
+import org.opencypher.v9_0.util.test_helpers.CypherFunSuite
+
+class SeqCombinerTest extends CypherFunSuite {
+
+  private val table = Seq(
+    Seq(Seq(1, 2, 3), Seq(4, 5), Seq(6)) ->
+      Seq(
+        Seq(1, 4, 6),
+        Seq(1, 5, 6),
+        Seq(2, 4, 6),
+        Seq(2, 5, 6),
+        Seq(3, 4, 6),
+        Seq(3, 5, 6)
+        ),
+    Seq(Seq(1, 2), Seq(3, 4)) ->
+      Seq(Seq(1, 3), Seq(1, 4), Seq(2, 3), Seq(2, 4)),
+    Seq(Seq(1, 2, 3), Seq.empty[Int], Seq(6)) -> Seq.empty,
+    Seq(Seq(1), Seq(2, 3), Seq(4, 5, 6)) ->
+      Seq(
+        Seq(1, 2, 4),
+        Seq(1, 2, 5),
+        Seq(1, 2, 6),
+        Seq(1, 3, 4),
+        Seq(1, 3, 5),
+        Seq(1, 3, 6)
+        ),
+  )
+
+  test("test combiners") {
+    table.foreach{
+      case (seq, expected) =>
+        combine(seq) should equal(expected)
+        combine(seq.map(_.toArray).toArray).map(_.toList).toList should equal(expected)
+    }
+  }
+}
