7ec92981e4f1e8e652b79f4add02aebdf6fe8db7
Pontus Melke <pontusmelke@gmail.com>
2020-03-06 17:10:17 +0100

    Remove assert with lambdas

--- frontend/src/main/scala/org/opencypher/v9_0/frontend/phases/Transformer.scala
+++ frontend/src/main/scala/org/opencypher/v9_0/frontend/phases/Transformer.scala
@@ -17,7 +17,7 @@ package org.opencypher.v9_0.frontend.phases
 
 import org.opencypher.v9_0.ast.prettifier.ExpressionStringifier
 import org.opencypher.v9_0.ast.prettifier.Prettifier
-import org.opencypher.v9_0.util.AssertionUtils.ifAssertionsEnabled
+import org.opencypher.v9_0.macros.AssertMacros.checkOnlyWhenAssertionsAreEnabled
 
 trait Transformer[-C <: BaseContext, -FROM, TO] {
   def transform(from: FROM, context: C): TO
@@ -31,8 +31,8 @@ trait Transformer[-C <: BaseContext, -FROM, TO] {
 }
 
 object Transformer {
-  val identity = new Transformer[BaseContext, Unit, Unit] {
-    override def transform(from: Unit, context: BaseContext) = ()
+  val identity: Transformer[BaseContext, Unit, Unit] = new Transformer[BaseContext, Unit, Unit] {
+    override def transform(from: Unit, context: BaseContext): Unit = ()
 
     override def name: String = "identity"
   }
@@ -41,8 +41,8 @@ object Transformer {
     * Transformer that can be inserted when debugging, to help detect
     * what part of the compilation that introduces an ast issue.
     */
-  def printAst(tag: String) = new Transformer[BaseContext, BaseState, BaseState] {
-    override def transform(from: BaseState, context: BaseContext) = {
+  def printAst(tag: String): Transformer[BaseContext, BaseState, BaseState] = new Transformer[BaseContext, BaseState, BaseState] {
+    override def transform(from: BaseState, context: BaseContext): BaseState = {
       println("     |||||||| PRINT AST: "+tag)
       println(Prettifier(ExpressionStringifier()).asString(from.maybeStatement.get))
       from
@@ -58,14 +58,14 @@ class PipeLine[-C <: BaseContext, FROM, MID, TO](first: Transformer[C, FROM, MID
     val step1 = first.transform(from, context)
 
     // Checking conditions inside assert so they are not run in production
-    ifAssertionsEnabled(accumulateAndCheckConditions(step1, first))
+    checkOnlyWhenAssertionsAreEnabled(accumulateAndCheckConditions(step1, first))
     val step2 = after.transform(step1, context)
-    ifAssertionsEnabled(accumulateAndCheckConditions(step2, after))
+    checkOnlyWhenAssertionsAreEnabled(accumulateAndCheckConditions(step2, after))
 
     step2
   }
 
-  private def accumulateAndCheckConditions[D <: C, STATE](from: STATE, transformer: Transformer[D, _, _]): Unit = {
+  private def accumulateAndCheckConditions[D <: C, STATE](from: STATE, transformer: Transformer[D, _, _]): Boolean = {
     (from, transformer) match {
       case (f: BaseState, phase: Phase[_, _, _]) =>
         val conditions = f.accumulatedConditions ++ phase.postConditions
@@ -75,6 +75,7 @@ class PipeLine[-C <: BaseContext, FROM, MID, TO](first: Transformer[C, FROM, MID
         }
       case _ =>
     }
+    true
   }
 
   override def name: String = first.name + ", " + after.name
