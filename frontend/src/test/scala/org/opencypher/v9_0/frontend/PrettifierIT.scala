7b95d0874d1a1e820e2c502e29583ea1a263bb5f
Sascha Peukert <sascha.peukert@neotechnology.com>
2020-03-18 15:54:41 +0100

    Make the user name optional in SHOW USER user PRIVILEGES

--- frontend/src/test/scala/org/opencypher/v9_0/frontend/PrettifierIT.scala
+++ frontend/src/test/scala/org/opencypher/v9_0/frontend/PrettifierIT.scala
@@ -469,6 +469,9 @@ class PrettifierIT extends CypherFunSuite {
       "show user `$user` privileges" ->
         "SHOW USER `$user` PRIVILEGES",
 
+      "show user privileges" ->
+        "SHOW USER PRIVILEGES",
+
       "show role abc privileges" ->
         "SHOW ROLE abc PRIVILEGES",
 
--- parser/src/test/scala/org/opencypher/v9_0/parser/privilege/PrivilegeAdministrationCommandParserTest.scala
+++ parser/src/test/scala/org/opencypher/v9_0/parser/privilege/PrivilegeAdministrationCommandParserTest.scala
@@ -35,11 +35,15 @@ class PrivilegeAdministrationCommandParserTest extends AdministrationCommandPars
   }
 
   test("SHOW USER user PRIVILEGES") {
-    yields(ast.ShowPrivileges(ast.ShowUserPrivileges("user") _))
+    yields(ast.ShowPrivileges(ast.ShowUserPrivileges(Some("user")) _))
   }
 
   test("SHOW USER `us%er` PRIVILEGES") {
-    yields(ast.ShowPrivileges(ast.ShowUserPrivileges("us%er") _))
+    yields(ast.ShowPrivileges(ast.ShowUserPrivileges(Some("us%er")) _))
+  }
+
+  test("SHOW USER PRIVILEGES") {
+    yields(ast.ShowPrivileges(ast.ShowUserPrivileges(None) _))
   }
 
   test("SHOW ROLE role PRIVILEGES") {
@@ -63,10 +67,6 @@ class PrivilegeAdministrationCommandParserTest extends AdministrationCommandPars
   }
 
   test("SHOW privalages") {
-    failsToParse
-  }
-
-  test("SHOW USER PRIVILEGES") {
     failsToParse
   }
 
