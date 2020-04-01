8c7fd7675552765791ae44bdb6f1c05b6659e1b4
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2020-04-01 17:11:21 +0200

    Move all rewrites to after Fabric planning (except for those essential for fabric analysis)
    Co-authored-by: Therese Magnusson <therese.magnusson@neotechnology.com>

---
+++ frontend/src/main/scala/org/opencypher/v9_0/frontend/phases/ExpandStarRewriter.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2002-2020 Neo4j Sweden AB (http://neo4j.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opencypher.v9_0.frontend.phases
+
+import org.opencypher.v9_0.frontend.phases.CompilationPhaseTracer.CompilationPhase.AST_REWRITE
+import org.opencypher.v9_0.rewriting.conditions.containsNoReturnAll
+import org.opencypher.v9_0.rewriting.rewriters.expandStar
+
+object ExpandStarRewriter extends Phase[BaseContext, BaseState, BaseState] {
+
+  def phase: CompilationPhaseTracer.CompilationPhase = AST_REWRITE
+
+  def description: String = "expand *"
+
+  def process(from: BaseState, context: BaseContext): BaseState =
+    from.withStatement(from.statement().endoRewrite(expandStar(from.semantics())))
+
+  override def postConditions: Set[Condition] =
+    Set(StatementCondition(containsNoReturnAll))
+}
