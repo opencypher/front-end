3f54248c5421a3e346cb9beb9ab08c147b6b0833
Satia Herfert <satiaherfert@gmx.de>
2020-09-23 09:57:42 +0200

    Fix scoping issue with nested pattern comprehensions.

--- expressions/src/main/scala/org/opencypher/v9_0/expressions/ExistsSubClause.scala
+++ expressions/src/main/scala/org/opencypher/v9_0/expressions/ExistsSubClause.scala
@@ -18,13 +18,13 @@ package org.opencypher.v9_0.expressions
 import org.opencypher.v9_0.util.InputPosition
 
 case class ExistsSubClause(pattern: Pattern, optionalWhereExpression: Option[Expression])
-                          (val position: InputPosition, val outerScope: Set[Variable]) extends ScopeExpression {
+                          (val position: InputPosition, override val outerScope: Set[Variable]) extends ScopeExpression with ExpressionWithOuterScope {
 
   self =>
 
   val patternElements: Seq[PatternElement] = pattern.patternParts.map(_.element)
 
-  def withOuterScope(outerScope: Set[Variable]): ExistsSubClause = copy()(position, outerScope)
+  override def withOuterScope(outerScope: Set[Variable]): ExistsSubClause = copy()(position, outerScope)
 
   override val introducedVariables: Set[LogicalVariable] = patternElements.collect { case e => e.allVariables }.flatten.toSet -- outerScope
 
