085a53d94177b90bfbde59c65976ca2a0e25a638
Therese Magnusson <scout.therese@gmail.com>
2020-10-13 10:31:13 +0200

    Address review comments

--- ast/src/main/scala/org/opencypher/v9_0/ast/SchemaCommand.scala
+++ ast/src/main/scala/org/opencypher/v9_0/ast/SchemaCommand.scala
@@ -49,6 +49,7 @@ case class CreateIndex(variable: Variable, label: LabelName, properties: List[Pr
   override def semanticCheck: SemanticCheck = ifExistsDo match {
     case IfExistsInvalidSyntax | IfExistsReplace => SemanticError(s"Failed to create index: `OR REPLACE` cannot be used together with this command.", position)
     case _ =>
+      // The validation of the values (provider, config keys and config values) are done at runtime.
       if (options.filterKeys(k => !k.equalsIgnoreCase("indexProvider") && !k.equalsIgnoreCase("indexConfig")).nonEmpty)
         SemanticError(s"Failed to create index: Invalid option provided, valid options are `indexProvider` and `indexConfig`.", position)
       else
@@ -150,6 +151,7 @@ case class CreateNodeKeyConstraint(variable: Variable, label: LabelName, propert
   override def semanticCheck: SemanticCheck =  ifExistsDo match {
     case IfExistsInvalidSyntax | IfExistsReplace => SemanticError(s"Failed to create node key constraint: `OR REPLACE` cannot be used together with this command.", position)
     case _ =>
+      // The validation of the values (provider, config keys and config values) are done at runtime.
       if (options.filterKeys(k => !k.equalsIgnoreCase("indexProvider") && !k.equalsIgnoreCase("indexConfig")).nonEmpty)
         SemanticError(s"Failed to create node key constraint: Invalid option provided, valid options are `indexProvider` and `indexConfig`.", position)
       else
@@ -167,6 +169,7 @@ case class CreateUniquePropertyConstraint(variable: Variable, label: LabelName, 
   override def semanticCheck: SemanticCheck =  ifExistsDo match {
     case IfExistsInvalidSyntax | IfExistsReplace => SemanticError(s"Failed to create uniqueness constraint: `OR REPLACE` cannot be used together with this command.", position)
     case _ =>
+      // The validation of the values (provider, config keys and config values) are done at runtime.
       if (options.filterKeys(k => !k.equalsIgnoreCase("indexProvider") && !k.equalsIgnoreCase("indexConfig")).nonEmpty)
         SemanticError(s"Failed to create uniqueness constraint: Invalid option provided, valid options are `indexProvider` and `indexConfig`.", position)
       else
