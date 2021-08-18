beb776fd890cd1621cd9836de07c51e12a7a8b73
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2021-08-18 08:37:25 +0200

    Address review comments

--- frontend/src/test/scala/org/opencypher/v9_0/frontend/SemanticAnalysisTest.scala
+++ frontend/src/test/scala/org/opencypher/v9_0/frontend/SemanticAnalysisTest.scala
@@ -401,7 +401,7 @@ class SemanticAnalysisTest extends CypherFunSuite {
     pipeline.transform(startState, context)
 
     context.errors.map(_.msg) shouldBe Seq(
-      "Node pattern predicates are not allowed in CREATE"
+      "Node pattern predicates are not allowed in CREATE, but only in MATCH clause or inside a pattern comprehension"
     )
   }
 
@@ -413,7 +413,7 @@ class SemanticAnalysisTest extends CypherFunSuite {
     pipeline.transform(startState, context)
 
     context.errors.map(_.msg) shouldBe Seq(
-      "Node pattern predicates are not allowed in MERGE"
+      "Node pattern predicates are not allowed in MERGE, but only in MATCH clause or inside a pattern comprehension"
     )
   }
 
@@ -449,7 +449,7 @@ class SemanticAnalysisTest extends CypherFunSuite {
     pipeline.transform(startState, context)
 
     context.errors.map(_.msg) shouldBe Seq(
-      "Node pattern predicates are not allowed in expression"
+      "Node pattern predicates are not allowed in expression, but only in MATCH clause or inside a pattern comprehension"
     )
   }
 
@@ -495,7 +495,7 @@ class SemanticAnalysisTest extends CypherFunSuite {
     pipeline.transform(startState, context)
 
     context.errors.map(_.msg) shouldBe Seq(
-      "Node pattern predicates are not allowed in expression"
+      "Node pattern predicates are not allowed in expression, but only in MATCH clause or inside a pattern comprehension"
     )
   }
 
