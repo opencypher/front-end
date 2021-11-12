neo4j/neo4j

Only parse node pattern predicate for named nodes
Fixes a JavaCC warning

community/cypher/front-end/parser/src/main/scala/org/neo4j/cypher/internal/parser/Patterns.scala
@@ -120,8 +120,10 @@ trait Patterns extends Parser
   )
 
   private def NodePattern: Rule1[expressions.NodePattern] = rule("a node pattern") (
-    group("(" ~~ MaybeVariable ~ MaybeNodeLabels ~ MaybeProperties ~ MaybeWereSubClause ~~ ")") ~~>>
-      { (v, labels, props, where) => expressions.NodePattern(v, labels, props, where)}
+    group("(" ~~ Variable ~ MaybeNodeLabels ~ MaybeProperties ~ MaybeWereSubClause ~~ ")") ~~>>
+      { (v, labels, props, where) => expressions.NodePattern(Some(v), labels, props, where)}
+    | group("(" ~~ MaybeNodeLabels ~ MaybeProperties ~~ ")") ~~>>
+      { (labels, props) => expressions.NodePattern(None, labels, props, None)}
     | group(Variable ~ MaybeNodeLabels ~ MaybeProperties)  ~~>>
       (expressions.InvalidNodePattern(_, _, _)) // Here to give nice error messages
   )

opencypher/front-end

parser/src/main/scala/org/opencypher/v9_0/parser/Patterns.scala
@@ -121,8 +121,10 @@ trait Patterns extends Parser
   )
 
   private def NodePattern: Rule1[expressions.NodePattern] = rule("a node pattern") (
-    group("(" ~~ MaybeVariable ~ MaybeNodeLabels ~ MaybeProperties ~ MaybeWhereSubClause ~~ ")") ~~>>
-      { (v, labels, props, where) => expressions.NodePattern(v, labels, props, where)}
+    group("(" ~~ Variable ~ MaybeNodeLabels ~ MaybeProperties ~ MaybeWhereSubClause ~~ ")") ~~>>
+      { (v, labels, props, where) => expressions.NodePattern(Some(v), labels, props, where)}
+    | group("(" ~~ MaybeNodeLabels ~ MaybeProperties ~~ ")") ~~>>
+      { (labels, props) => expressions.NodePattern(None, labels, props, None)}
     | group(Variable ~ MaybeNodeLabels ~ MaybeProperties)  ~~>>
       (expressions.InvalidNodePattern(_, _, _)) // Here to give nice error messages
   )