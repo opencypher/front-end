2acbab36191a193375b155666070c677dae42511
Daniel Leaver <daniel.leaver@neotechnology.com>
2022-03-25 14:01:23 +0000

    Pretty index hints (#14803)
    * Move Prettifier integration tests to their own package
    
    These 4 files prettifier test files were bundled with some unrelated tests in the cypher/frontend/test folder, so I moved them to cypher/frontend/test/prettifier
    
    * Add Index Hint prettifier tests
    
    These tests should have been added at some point in the past, but we have only recently noticed that they were missing.
    
    * Move USING..INDEX tests to a different place
    
    Moving USING [index_type] INDEX tests to the queryTests() method

--- frontend/src/test/scala/org/opencypher/v9_0/frontend/prettifier/ExpressionStringifierIT.scala
+++ frontend/src/test/scala/org/opencypher/v9_0/frontend/prettifier/ExpressionStringifierIT.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.opencypher.v9_0.frontend
+package org.opencypher.v9_0.frontend.prettifier
 
 import org.opencypher.v9_0.ast.factory.neo4j.JavaccRule
 import org.opencypher.v9_0.ast.prettifier.ExpressionStringifier
